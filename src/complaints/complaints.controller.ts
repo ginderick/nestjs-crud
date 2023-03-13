74aba40190dded47a72d95f53f2582f41694ad84
Gin Derick <ginderick@pop-os.localdomain>
2023-03-13 12:59:33 +0800

    update complaints controller and service

--- src/complaints/complaints.controller.ts
+++ src/complaints/complaints.controller.ts
@@ -1,22 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ComplaintsService } from './complaints.service';
 
 @Controller()
 export class ComplaintsController {
+  constructor(private complaintsService: ComplaintsService) {}
+
   @Get('complaints')
   getComplaints() {
-    return [
-      {
-        ticket_id: '1677830978',
-        sender_id: '12345678',
-        address: 'Laguna',
-        branch: 'Laguna',
-        contact: '+639272792254',
-        model: 'FZI',
-        remarks: null,
-        tag: 'parts',
-        ticket_status: 'CRITICAL',
-        concerns: null,
-      },
-    ];
+    return this.complaintsService.getComplaints();
   }
 }
--- src/complaints/complaints.service.ts
+++ src/complaints/complaints.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ComplaintsService {}
+export class ComplaintsService {
+  async getComplaints() {
+    return [
+      {
+        ticket_id: '1677830978',
+        sender_id: '12345678',
+        address: 'Laguna',
+        branch: 'Laguna',
+        contact: '+639272792254',
+        model: 'FZI',
+        remarks: null,
+        tag: 'parts',
+        ticket_status: 'CRITICAL',
+        concerns: null,
+      },
+    ];
+  }
+}
