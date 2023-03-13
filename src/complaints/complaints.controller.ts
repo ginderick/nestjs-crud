5cfdffe918c20012a856495f6df1917d3788253e
Gin Derick <ginderick@pop-os.localdomain>
2023-03-13 11:23:44 +0800

    update complaints controller and add swagger module

--- src/complaints/complaints.controller.ts
+++ src/complaints/complaints.controller.ts
@@ -4,6 +4,19 @@ import { Controller, Get } from '@nestjs/common';
 export class ComplaintsController {
   @Get('complaints')
   getComplaints() {
-    return 'Complaints';
+    return [
+      {
+        ticket_id: '1677830978',
+        sender_id: '12345678',
+        address: 'Laguna',
+        branch: 'Laguna',
+        contact: '+639272792254',
+        model: 'FZI',
+        remarks: null,
+        tag: 'parts',
+        ticket_status: 'CRITICAL',
+        concerns: null,
+      },
+    ];
   }
 }
