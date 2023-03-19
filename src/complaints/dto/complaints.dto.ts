39b5dc20ddf5fdc8dc91f1a2ab68968d77cdac27
Gin Derick <ginderick@pop-os.localdomain>
2023-03-19 18:35:07 +0800

    update createComplaints schema and dto

--- src/complaints/dto/complaints.dto.ts
+++ src/complaints/dto/complaints.dto.ts
@@ -1,9 +1,11 @@
 export class CreateComplaintsDto {
   sender_id: number;
+  name: string;
   address: string;
   contact: string;
   model: string;
   remarks: string;
+  message: string;
   tag: string;
   ticket_status: string;
   concerns: string;
--- src/complaints/schema/complaints.schema.ts
+++ src/complaints/schema/complaints.schema.ts
@@ -2,11 +2,13 @@ import { z } from 'zod';
 
 export const createComplaintsSchema = z.object({
   sender_id: z.number(),
-  contact: z.string(),
+  name: z.string(),
   address: z.string(),
+  contact: z.string(),
   model: z.string(),
-  remarks: z.string(),
+  message: z.string(),
+  remarks: z.string().optional(),
   tag: z.string(),
   ticket_status: z.string(),
-  concerns: z.string(),
+  concerns: z.string().optional(),
 });
