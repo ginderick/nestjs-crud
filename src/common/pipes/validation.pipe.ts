d6c264389328c15dcbbb97efcb2cfbad5fef77f8
Gin Derick <ginderick@pop-os.localdomain>
2023-03-19 15:07:21 +0800

    add zod validation pipe

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { ObjectSchema } from 'joi';
+import { ZodTypeAny } from 'zod';
 
 @Injectable()
 export class JoiValidationPipe implements PipeTransform {
@@ -9,6 +10,19 @@ export class JoiValidationPipe implements PipeTransform {
     const { error } = this.schema.validate(value);
     if (error) {
       throw new BadRequestException('Validation Failed', `${error}`);
+    }
+    return value;
+  }
+}
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodTypeAny) {}
+
+  transform(value: any) {
+    const result = this.schema.safeParse(value);
+    if (result.success === false) {
+      throw new BadRequestException('Validation Failed', `${result.error}`);
     }
     return value;
   }
