4bb1a92622592952e7aaa02f8aa9582eab37a40c
Gin Derick <ginderick@pop-os.localdomain>
2023-03-20 22:33:51 +0800

    update filterComplaints query params to be optional

--- src/complaints/complaints.controller.ts
+++ src/complaints/complaints.controller.ts
@@ -1,5 +1,10 @@
-import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
-import { ZodValidationPipe } from 'src/common/pipes/validation.pipe';
+import { Body, Controller, Get, Post, Query, UsePipes } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+import {
+  TagValidationPipe,
+  TicketStatusValidationPipe,
+  ZodValidationPipe,
+} from 'src/common/pipes/validation.pipe';
 import { ComplaintsService } from './complaints.service';
 import { CreateComplaintsDto } from './dto';
 import { createComplaintsSchema } from './schema/complaints.schema';
@@ -17,5 +22,15 @@ export class ComplaintsController {
   @UsePipes(new ZodValidationPipe(createComplaintsSchema))
   async createComplaint(@Body() complaintsDto: CreateComplaintsDto) {
     return await this.complaintsService.createComplaints(complaintsDto);
+  }
+
+  @ApiQuery({ name: 'tag', required: false, type: String })
+  @ApiQuery({ name: 'ticket_status', required: false, type: String })
+  @Get('filter-results')
+  async filterComplaints(
+    @Query('tag', TagValidationPipe) tag?: string,
+    @Query('ticket_status', TicketStatusValidationPipe) ticket_status?: string,
+  ) {
+    return { ticket_status };
   }
 }
